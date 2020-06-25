217c67ab08f5050837f346f6d4a59d6632715c15
LincanYang <904725797@qq.com>
2020-06-25 17:46:35 +0800

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,25 @@
+#define IDD_REACTIVITYRATIOESTIMATE_DIALOG 102
+#define IDR_MAINFRAME                   128
+#define IDD_PARA_DLG                    130
+#define IDC_PARA_BTN                    1000
+#define IDC_START_BTN                   1001
+#define IDC_EDIT                        1002
+#define IDC_FILE_BTN                    1003
+#define IDC_RA_LEDIT                    1004
+#define IDC_RA_REDIT                    1005
+#define IDC_RB_LEDIT                    1006
+#define IDC_RB_REDIT                    1007
+#define IDC_MIN_EDIT_RA                 1008
+#define IDC_PATH_EDIT                   1009
+#define IDC_MIN_EDIT_RB                 1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        134
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
