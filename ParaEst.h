9cf50d37996e4ea541f8c19a5c5bd336c2f49cb6
LincanYang <904725797@qq.com>
2020-06-25 17:41:33 +0800

    Update ParaEst.h

--- ParaEst.h
+++ ParaEst.h
@@ -4,14 +4,29 @@
 #include "tool.h"
 #include <thread>
 
+#define STEPS 100
+#define TOTAL_PROBABILITY 1
+
+typedef struct rArBFAYr
+{
+	double m_rA;
+	double m_rB;
+	vector<double>* m_pFA_V;
+	double m_YrVal;
+	double m_DfVal;
+	rArBFAYr(double rA, double rB) :m_rA(rA), m_rB(rB), m_pFA_V(NULL), m_YrVal(0), m_DfVal(0){}
+} rArBFAYr;
+
 class ParaEst
 {
 public:
-	ParaEst(const string& strSimuFile);
+	ParaEst(const string& strSimuFile, ScanFile& scanFile);
 	~ParaEst();
+	double rA_Aver, rB_Aver;
+	double rA_MaxLikelihood, rB_MaxLikelihood;
 private:
 	string m_strSimuFile;
-	ScanFile scanFile;
+	//ScanFile scanFile;
 	double minStepSizeRA;
 	double minStepSizeRB;
 	vector<thread*> _thread_Vec;
@@ -40,6 +55,7 @@ private:
 	void DeletepFA_Vec(vector<rArBFAYr*>* pFA_Vec);
 	void Cal_F_for_fA_Vec_ML(double rA, double rB);
 	void PostProba_YrVal(double stepSize_rA, double stepSize_rB, vector<rArBFAYr*>* pFcalA_V);
+	void WriteToFile(vector<rArBFAYr*>* pFcalA_V, string appx = "");
 	ddPair StepSizerArB();
 	void OnMultiThreads(ddPair stepSize);
 	void Loop_M_L(ddPair stepSize);
