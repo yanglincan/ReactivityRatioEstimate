6cce750212981a98b1b421ce398bd6cc6e6beb4e
LincanYang <904725797@qq.com>
2020-06-25 17:43:28 +0800

    Add files via upload

---
+++ CParas.cpp
@@ -0,0 +1,74 @@
+﻿#include "pch.h"
+#include "ReactivityRatioEstimate.h"
+#include "CParas.h"
+#include "afxdialogex.h"
+
+IMPLEMENT_DYNAMIC(CParas, CDialogEx)
+
+CParas::CParas(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_PARA_DLG, pParent)
+	, m_rA_LeftRange(0)
+	, m_rA_RightRange(1)
+	, m_rB_LeftRange(0)
+	, m_rB_RightRange(1)
+	, m_MinStepLengthRA(0)
+	, m_MinStepLengthRB(0)
+	, m_FilePathName(_T(""))
+{
+}
+
+CParas::~CParas()
+{
+}
+
+
+void CParas::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_RA_LEDIT, m_rA_LeftRange);
+	DDX_Text(pDX, IDC_RA_REDIT, m_rA_RightRange);
+	DDX_Text(pDX, IDC_RB_LEDIT, m_rB_LeftRange);
+	DDX_Text(pDX, IDC_RB_REDIT, m_rB_RightRange);
+	DDX_Text(pDX, IDC_MIN_EDIT_RA, m_MinStepLengthRA);
+	DDX_Text(pDX, IDC_PATH_EDIT, m_FilePathName);
+	DDX_Text(pDX, IDC_MIN_EDIT_RB, m_MinStepLengthRB);
+}
+
+
+BEGIN_MESSAGE_MAP(CParas, CDialogEx)
+	ON_BN_CLICKED(IDC_FILE_BTN, &CParas::OnBnClickedFileBtn)
+	ON_BN_CLICKED(IDOK, &CParas::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// CParas 消息处理程序
+void CParas::OnBnClickedFileBtn()
+{
+	if (FALSE == UpdateData(TRUE))	//data is being retrieved (TRUE).
+		return;
+	CFileDialog fd(TRUE);
+	if (IDCANCEL == fd.DoModal())
+		return;
+	m_FilePathName = fd.GetPathName();
+	UpdateData(FALSE);	//dialog box is being initialized (FALSE)
+}
+
+void CParas::OnBnClickedOk()
+{
+	if (FALSE == UpdateData(TRUE))	//data is being retrieved (TRUE).
+		return;
+	if (m_rA_LeftRange >= m_rA_RightRange || m_rB_LeftRange >= m_rB_RightRange){
+		MessageBox(_T("Wrong range!"), _T("💀💀💀"));
+		return;
+	}
+	if (m_MinStepLengthRA < 0 || m_MinStepLengthRB < 0) {
+		MessageBox(_T("Wrong step-length (🏃‍)!"), _T("💀💀💀"));
+		return;
+	}
+	if (m_FilePathName.IsEmpty()) {
+		MessageBox(_T("No file is open!"), _T("💀💀💀"));
+		return;
+	}
+	CDialogEx::OnOK();
+}
+
---
+++ CParas.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+class CParas : public CDialogEx
+{
+	DECLARE_DYNAMIC(CParas)
+public:
+	CParas(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~CParas();
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PARA_DLG };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	DECLARE_MESSAGE_MAP()
+public:
+	double m_rA_LeftRange;	//rA 范围的左边界
+	double m_rA_RightRange;	//rA 范围的右边界
+	double m_rB_LeftRange;	//rB 范围的左边界
+	double m_rB_RightRange;	//rB 范围的右边界
+	double m_MinStepLengthRA;	//rA最小步长
+	double m_MinStepLengthRB;	//rB最小步长
+	CString m_FilePathName;	//输入文件的名称，用于确定输出文件的名称
+public:
+	afx_msg void OnBnClickedFileBtn();
+	afx_msg void OnBnClickedOk();
+};
